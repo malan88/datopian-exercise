fd6b8c7852012f5ded59838102dfc5421bba9f66
malan88 <m@stdwtr.io>
2020-11-03 15:59:53 -0500

    Finalize

--- scrape.py
+++ scrape.py
@@ -1,22 +1,21 @@
-from bs4 import BeautifulSoup
-import urllib3
 import csv
 from datetime import datetime, timedelta
-from pprint import PrettyPrinter
+from bs4 import BeautifulSoup
+import urllib3
 
 http = urllib3.PoolManager()
+
 URL = 'https://www.eia.gov/dnav/ng/hist/rngwhhdD.htm'
-
 # Summary is an identifier unique to target table
-SUMMARY = "Henry Hub Natural Gas Spot Price (Dollars per Million Btu)"
-
-pp = PrettyPrinter()
+IDENTIFIER = {
+    'summary': "Henry Hub Natural Gas Spot Price (Dollars per Million Btu)"
+}
 
 
 def get():
     data = http.request('GET', URL)
     soup = BeautifulSoup(data.data, 'html.parser')
-    table = soup.find('table', {'summary': SUMMARY})
+    table = soup.find('table', IDENTIFIER)
     return table
 
 
@@ -27,27 +26,28 @@ def convert(soup):
         for d in r.find_all('td'):
             row.append(d.string)
         toplevel.append(row)
-
-    # filter out empty rows
+    # filter out empty rows, kinda hackish, probably slow
     toplevel = list(
         filter(lambda l: bool(list(filter(None, l))),
                toplevel)
     )
+    # pop the header
     toplevel.pop(0)
     return toplevel
 
 
 def normalize(table):
     newtable = []
-
     for row in table:
+        # split the date on to, only take the first half, remove nbsp's
         rawdate = row[0].split(' to ')[0].replace('\xa0\xa0', '')
+        # 0-pad the day
         prepped_date = rawdate.replace('- ', '-0')
+        # convert to date object
         date = datetime.strptime(prepped_date, '%Y %b-%d').date()
         for i, price in enumerate(row[1:]):
             currentdate = date + timedelta(days=i)
             newtable.append([currentdate.strftime('%Y-%m-%d'), price])
-
     return newtable
 
 
@@ -63,6 +63,7 @@ def main():
     table = convert(table)
     table = normalize(table)
     writecsv(table)
+
 
 if __name__ == "__main__":
     main()
