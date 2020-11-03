beb6222f64ab1c207b0e98effe59b0217a4aa877
malan88 <m@stdwtr.io>
2020-11-03 15:24:04 -0500

    Getting data ready

--- scrape.py
+++ scrape.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup
 import urllib3
 import csv
+from pprint import PrettyPrinter
 
 http = urllib3.PoolManager()
 URL = 'https://www.eia.gov/dnav/ng/hist/rngwhhdD.htm'
 
 # Summary is an identifier unique to target table
 SUMMARY = "Henry Hub Natural Gas Spot Price (Dollars per Million Btu)"
+
+pp = PrettyPrinter()
 
 
 def gettable():
@@ -21,17 +24,24 @@ def converttable(soup):
     for r in soup.find_all('tr'):
         row = []
         for d in r.find_all('td'):
-            row.append(str(d))
+            row.append(d.string)
         toplevel.append(row)
-
+    toplevel = list(filter(lambda l: bool(list(filter(None, l))), toplevel))
     return toplevel
 
+
+def writecsv(table):
+    with open('data.csv', 'w', newline='') as fout:
+        writer = csv.writer(fout, dialect='excel')
+        for row in table:
+            writer.writerow(row)
 
 
 def main():
     table = gettable()
     table = converttable(table)
-    print(table)
+    pp.pprint(table)
+    writecsv(table)
 
 if __name__ == "__main__":
     main()
